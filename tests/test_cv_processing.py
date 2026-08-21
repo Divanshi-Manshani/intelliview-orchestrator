706973290599e38d4a6c8ba4e8a08179a0935154
kottenikhil54141 <kottenikhil499@gmail.com>
2026-08-21 08:41:34 +0530

    style: format CV processing tests

--- tests/test_cv_processing.py
+++ tests/test_cv_processing.py
@@ -19,9 +19,7 @@ def test_missing_frame_is_handled_and_logged(caplog):
 
 def test_invalid_file_path_is_handled_and_logged(caplog):
     with caplog.at_level("WARNING"):
-        result = detect_faces_in_frame(
-            frame_path="/tmp/this-file-does-not-exist.jpg"
-        )
+        result = detect_faces_in_frame(frame_path="/tmp/this-file-does-not-exist.jpg")
 
     assert result is None
     assert "Unable to decode video frame" in caplog.text
